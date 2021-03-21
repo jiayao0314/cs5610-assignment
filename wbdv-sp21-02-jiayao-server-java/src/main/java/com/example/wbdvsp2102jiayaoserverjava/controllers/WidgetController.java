cffd65b6f79c5e4475c3f2c0660a112c7b9565a7
jiayao0314 <li.jiayao@northeastern.edu>
2021-03-21 08:48:45 -0700

    assignment 5 server

---
+++ wbdv-sp21-02-jiayao-server-java/src/main/java/com/example/wbdvsp2102jiayaoserverjava/controllers/WidgetController.java
@@ -0,0 +1,55 @@
+package com.example.wbdvsp2102jiayaoserverjava.controllers;
+
+import com.example.wbdvsp2102jiayaoserverjava.models.Widget;
+import com.example.wbdvsp2102jiayaoserverjava.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+    @Autowired
+    WidgetService service;// = new WidgetService();
+
+    @PostMapping("/api/topics/{tid}/widgets")
+    public Widget createWidget(
+            @PathVariable("tid") String topicId,
+            @RequestBody Widget widget
+    ) {
+        return service.createWidgetForTopic(topicId, widget);
+    }
+
+    @GetMapping("/api/topics/{tid}/widgets")
+    public List<Widget> findWidgetsForTopic(
+            @PathVariable("tid") String topicId
+    ) {
+        return service.findWidgetsForTopic(topicId);
+    }
+
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/api/widgets/{wid}")
+    public Widget findWidgetById(
+            @PathVariable("wid") Long id) {
+        return service.findWidgetById(id);
+    }
+
+    @DeleteMapping("/api/widgets/{wid}")
+    public Integer deleteWidget(
+            @PathVariable("wid") Long id
+    ) {
+        return service.deleteWidget(id);
+    }
+
+    @PutMapping("/api/widgets/{wid}")
+    public Integer updateWidget(
+            @PathVariable("wid") Long id,
+            @RequestBody Widget widget) {
+        return service.updateWidget(id, widget);
+    }
+}
---
+++ wbdv-sp21-02-jiayao-server-java/src/main/java/com/example/wbdvsp2102jiayaoserverjava/models/Widget.java
@@ -0,0 +1,70 @@
+package com.example.wbdvsp2102jiayaoserverjava.models;
+
+public class Widget {
+    private Long id;
+    private String type;
+    private Integer size;
+    private String text;
+    private String topicId; //
+
+    private String name;
+    private int widgetOrder;
+    private String url;
+    private int width;
+    private int height;
+    private String cssClass;
+    private String style;
+    private String value;
+
+
+    public String getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Widget() {
+    }
+
+    public Widget(Long id, String topicId, String type, Integer size, String text) {
+        this.id = id;
+        this.topicId = topicId;
+        this.type = type;
+        this.size = size;
+        this.text = text;
+    }
+}
